6c58ab0d2a26c8c45a82bd3136a3121d185f7401
romanboo <romanminarov@gmail.com>
2025-01-01 18:53:19 +0400

    added some files

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,14 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
+
     @Test
-    void successfulSearchTest() {
+    void successfulSearchTest_Firefox() {
+        Configuration.browser = "firefox";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+
+
+
+    @Test
+    void successfulSearchTest_Selenide() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldBe(visible, Duration.ofMillis(35000)).shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void successfulSearchTest_MosRu() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("mos.ru").pressEnter();
+        $("[id=search]").shouldBe(visible, Duration.ofMillis(35000)).shouldHave(text("mos.ru"));
     }
 }
